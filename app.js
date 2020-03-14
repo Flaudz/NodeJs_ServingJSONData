768896e5e4848b616c1f2eb7ce2ef8ae3444f687
Flaudz <nico936d@edu.campusvejle.dk>
2020-03-14 15:23:02 +0100

    Made my first json request or something

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const fs = require("fs");
 
 let server = http.createServer((req, res) => {
 	console.log(`Request was me: ${req.url}`);
-	res.writeHead(200, { "Content-Type": "text/html" });
+	res.writeHead(200, { "Content-Type": "application/json" });
+	let myObj = {
+		name: "Ryu",
+		job: "ninja",
+		age: 29
+	};
+	res.end(JSON.stringify(myObj));
 });
 
 server.listen(5050, "127.0.0.1");
